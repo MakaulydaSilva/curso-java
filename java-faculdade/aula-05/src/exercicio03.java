0fe97b429689cc859726f75be899c5a80156ed5e
MakaulydaSilva <makauly123@outlook.com>
2022-07-27 20:17:19 -0300

    att

---
+++ java-faculdade/aula-05/src/exercicio03.java
@@ -0,0 +1,12 @@
+
+public class exercicio03 {
+
+	public static void main(String[] args) {
+		int matriz [][] = {{1,1}, {2,2}, {3,3}};
+		for(int i = 0; i < matriz.length; i++) 
+		    for(int j = 0; j < matriz[i].length; j++) {
+		    	System.out.println(matriz[i][j]);
+		    }
+	}
+
+}
---
+++ java-faculdade/aula-05/src/lista.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class lista {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		
+		System.out.println(numbers);
+
+	}
+
+}
