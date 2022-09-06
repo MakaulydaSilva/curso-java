b6b1547852ec2219f8772f1b145a658cf8a98fc9
MakaulyDaSilva <makauly123@outlook.com>
2022-09-05 23:04:00 -0300

    exercicios

---
+++ curso-udemy/ex03/src/Exercicio01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numero;
+		
+		System.out.println("Digite um número:");
+		numero = sc.nextInt();
+		
+		if (numero < 0) {
+			System.out.println("NEGATIVO");
+		} else {
+			System.out.println("NÃO NEGATIVO");
+		}
+        
+		
+		sc.close();
+	}
+
+}
---
+++ curso-udemy/ex03/src/Exercicio02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num;
+		
+		System.out.println("Digite um número para saber se é par ou ímpar");
+		num = sc.nextInt();
+		
+		if(num % 2 == 0) {
+			System.out.println("PAR");
+		} else {
+			System.out.println("ÍMPAR");
+		}
+
+	}
+
+}
